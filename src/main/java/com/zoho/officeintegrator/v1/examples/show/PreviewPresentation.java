44448aa195798c5899146c5bba32b3a5ef2adfc9
prabakaran-zoho <155607009+prabakaran-zoho@users.noreply.github.com>
2024-08-02 15:27:33 +0530

    Office integrator sdk jar name changes (#1)
    * Latest officeintegrator java sdk changes
    
    * Readme files updated
    
    * Help documentation changes

--- src/main/java/com/zoho/officeintegrator/v1/examples/show/PreviewPresentation.java
+++ src/main/java/com/zoho/officeintegrator/v1/examples/show/PreviewPresentation.java
@@ -7,7 +7,8 @@ import java.util.logging.Level;
 import com.zoho.api.authenticator.Auth;
 import com.zoho.api.authenticator.Token;
 import com.zoho.officeintegrator.Initializer;
-import com.zoho.officeintegrator.dc.USDataCenter;
+import com.zoho.officeintegrator.dc.DataCenter;
+import com.zoho.officeintegrator.dc.Environment;
 import com.zoho.officeintegrator.logger.Logger;
 import com.zoho.officeintegrator.logger.Logger.Levels;
 import com.zoho.officeintegrator.util.APIResponse;
@@ -60,28 +61,36 @@ public class PreviewPresentation {
 		}
 	}
 	
+	//Initialize SDK on service start up once before making any api call to office integrator sdk.
 	public static boolean initializeSdk() {
 		boolean status = false;
 
 		try {
+			
+			//Sdk application log configuration
 			Logger logger = new Logger.Builder()
 			        .level(Levels.INFO)
 			        //.filePath("<file absolute path where logs would be written>") //No I18N
 			        .build();
 
 			List<Token> tokens = new ArrayList<Token>();
-			Auth auth = new Auth.Builder().addParam("apikey", "2ae438cf864488657cc9754a27daa480").authenticationSchema(new Authentication.TokenFlow()).build();
+			Auth auth = new Auth.Builder()
+				.addParam("apikey", "2ae438cf864488657cc9754a27daa480") //Update this apikey with your own apikey signed up in office inetgrator service
+				.authenticationSchema(new Authentication.TokenFlow())
+				.build();
 			
 			tokens.add(auth);
-			
+
+			Environment environment = new DataCenter.Production("https://api.office-integrator.com"); // Refer this help page for api end point domain details -  https://www.zoho.com/officeintegrator/api/v1/getting-started.html
+
 			new Initializer.Builder()
-				.environment(new USDataCenter.Production())
+				.environment(environment)
 				.tokens(tokens)
 				.logger(logger)
 				.initialize();
 			
 			status = true;
-} catch (Exception e) {
+		} catch (Exception e) {
 			LOGGER.log(Level.INFO, "Exception in creating document session url - ", e); //No I18N
 		}
 		return status;
